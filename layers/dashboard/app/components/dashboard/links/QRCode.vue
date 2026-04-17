14f94ebb3d154940ca5974e3b6ab3dc351d9a757
tm-huangwf <huangwf@truemetrics.cn>
2026-04-17 14:26:41 +0800

    Update QRCode.vue

--- layers/dashboard/app/components/dashboard/links/QRCode.vue
+++ layers/dashboard/app/components/dashboard/links/QRCode.vue
@@ -16,10 +16,10 @@ const options = {
   type: 'svg' as const,
   margin: 10,
   qrOptions: { typeNumber: 0 as const, mode: 'Byte' as const, errorCorrectionLevel: 'Q' as const },
-  imageOptions: { hideBackgroundDots: true, imageSize: 0.4, margin: 2 },
+  imageOptions: { hideBackgroundDots: true, imageSize: 0, margin: 2 },
   dotsOptions: { type: 'dots' as const, color: '#000000' },
   backgroundOptions: { color: '#ffffff' },
-  image: props.image,
+  image: '',
   dotsOptionsHelper: {
     colorType: { single: true, gradient: false },
     gradient: {
